825e4121837d8cac10be7d9cb87ee3ffc2651eea
Ignacio Severo Inga Castillo <iinga@Sodimac.com.pe>
2019-08-09 22:49:39 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ignacio:benBviUwBwDWAsHs@cluster0-tvhtu.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
